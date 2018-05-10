942f0cf69428fe7447379e9a733a296a5dda364c
webDevSaurav <webdevelopersaurav@gmail.com>
2018-05-11 03:10:43 +0530

    home page v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import {RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {"path" : "", component : HomeComponent, pathMatch : "full"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
